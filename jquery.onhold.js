761cb29154d062ecfd7cb25b850d49c435903bd9
Raúl Luna <raul_lun@msn.com>
2017-10-13 20:06:32 -0500

    Create jquery.onhold.js

---
+++ jquery.onhold.js
@@ -0,0 +1,34 @@
+//OnHold with Velocity
+$.fn.extend({
+    velocityControlTimeoutId: 0,
+    velocitySlowTimeout: 250,
+    velocityFastTimeout: 50,
+    velocityTicksBeforeFast: 3,
+    velocityTicksCount: 0,
+    makeTimeout: function () {
+        let thisObject = this;
+        this.velocityControlTimeoutId = setTimeout(function () {
+            thisObject.onHoldCallback();
+            thisObject.velocityTicksCount++;
+            thisObject.makeTimeout();
+        }, (this.velocityTicksCount>this.velocityTicksBeforeFast?this.velocityFastTimeout:this.velocitySlowTimeout));
+        return this;
+    },
+    onHoldCallback: $.noop(),
+    onHold: function (newCallback) {
+        let thisObject = this;
+        if (newCallback) {
+            this.onHoldCallback=newCallback;
+            this.on('mousedown', function () {
+                thisObject.velocityTicksCount = 0;
+                thisObject.makeTimeout();
+            }).on('mouseup mouseleave', function() {
+                clearTimeout(thisObject.velocityControlTimeoutId);
+            });
+        } else {
+            this.off('mousedown mouseup mouseleave');
+            this.onHoldCallback = $.noop();
+        }
+        return this;
+    }
+});
